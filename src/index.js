ceaa804961827996ef4500f9aaebfe181b2a64b4
Behruz <>
2023-07-07 17:25:03 +0500

    eslint

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import './index.scss';
-import './index.html';
-import {mult, sum} from './modules/calc';
-import image from './img/Screenshot_2023.07.05_19.55.47.208.png';
+import "./index.scss";
+import "./index.html";
+import { mult, sum } from "./modules/calc";
+import image from "./img/Screenshot_2023.07.05_19.55.47.208.png";
 
-const imgEl = document.querySelector('.img');
+const imgEl = document.querySelector(".img");
 
 const img = new Image();
 img.src = image;
@@ -12,4 +12,4 @@ img.width = 300;
 imgEl.append(img);
 
 console.log(mult(2, 4));
-console.log(sum(5, 4));
+console.log(sum(5, 10));
--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,7 +1,2 @@
-export const mult = (a, b)=> {
-    return a * b;
-}
-
-export const sum = async (a, b)=> {
-    return a + b;
-}
+export const mult = (a, b) => a * b;
+export const sum = async (a, b) => a + b;
