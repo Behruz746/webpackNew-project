a1e0d14fa1391e0b26f3e22a40465ce13e2d7843
Behruz <>
2023-07-07 12:58:49 +0500

    new function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import './index.scss';
 import './index.html';
+import {mult, sum} from './modules/calc';
+import image from './img/Screenshot_2023.07.05_19.55.47.208.png';
 
-const mult = (a, b)=> {
-    return a * b;
-}
+const imgEl = document.querySelector('.img');
+
+const img = new Image();
+img.src = image;
+
+img.width = 300;
+imgEl.append(img);
 
 console.log(mult(2, 4));
-console.log(mult(5, 4));
+console.log(sum(5, 4));
--- webpack.config.js
+++ webpack.config.js
@@ -10,14 +10,15 @@ const devtool = devMode ? 'source-map' : undefined; // agar project development 
 module.exports = {
     mode,
     target,
-    devtool,
+    devtool, 
 
-    entry: path.resolve(__dirname, 'src/index.js'),
+    entry: ["@babel/polyfill", path.resolve(__dirname, 'src/index.js')],
 
     output: {
         path: path.resolve(__dirname, 'dist'),
         clean: true,
         filename: '[name].[contenthash].js', // main.js ni harbita yangilanganida yandi nome beradi
+        assetModuleFilename: 'assets/[name][ext]'
     },
 
     plugins: [
@@ -26,7 +27,7 @@ module.exports = {
         }),
 
         new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].scss',
+            filename: '[name].[contenthash].css',
         }),
     ],
 
@@ -35,7 +36,7 @@ module.exports = {
             directory: path.resolve(__dirname, 'dist'),
         },
 
-        port: 5000,
+        port: 3000,
         open: true,
         hot: true,
         compress: true,
@@ -56,9 +57,73 @@ module.exports = {
                 devMode ? "style-loader" : MiniCssExtractPlugin.loader,
                 // Translates CSS into CommonJS
                 "css-loader",
+                {
+                    loader: "postcss-loader",
+                    options: {
+                        postcssOptions: {
+                            plugins: [require('postcss-preset-env')],
+                        },
+                    },
+                },
                 // Compiles Sass to CSS
                 "sass-loader",
               ],
+            },
+
+            {
+                test: /\.woff2?$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name][ext]',
+                }
+            },
+
+            { // bu imglarni mabayd larnin kichkina qilib beradi
+                test: /\.(jpe?g|png|webp|gif|svg)$/i,
+                use: [ 
+                    {
+                        loader: 'image-webpack-loader',
+
+                        options: {
+                            mozjpeg: {
+                              progressive: true,
+                            },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                              enabled: false,
+                            },
+                            pngquant: {
+                              quality: [0.65, 0.90],
+                              speed: 4
+                            },
+                            gifsicle: {
+                              interlaced: false,
+                            },
+                            // the webp option will enable WEBP
+                            webp: {
+                              quality: 75
+                            },
+                        },
+                    },
+                ],
+
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext]',
+                }
+            },
+
+            {
+                test: /\.(?:js|mjs|cjs)$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }],
+                        ],
+                    },
+                },
             },
         ],
     },
